118d3cee903ed4ee0f0549bdaf6d294f307becd1
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2016-11-25 11:33:03 +0100

    fix(sql)

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -13,9 +13,7 @@ CREATE TABLE PERSONNE (
   prenom varchar(15) NOT NULL,
   evaluation varchar(200) NOT NULL,
   pere varchar(100) NOT NULL
-);
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-/*
 ALTER TABLE PERSONNE
   ADD CONSTRAINT fk_personne_pere foreign key (pere) REFERENCES PERSONNE(id);
-*/
