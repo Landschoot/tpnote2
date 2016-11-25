2cc9b16d33e966f00c9f279768d305908a0411ef
landschoot <landschoot@b13p15.fil.univ-lille1.fr>
2016-11-25 11:51:47 +0100

    gui identification

--- src/main/java/domain/Personne.java
+++ src/main/java/domain/Personne.java
@@ -12,20 +12,12 @@ import java.util.List;
 @Data
 @Builder
 public class Personne {
-    private int id;
+    private String identifiant;
     private String nom;
     private String prenom;
     private String description;
     private Personne pere;
     private List<Personne> fils;
-
-    public Personne(int id, String nom, String prenom, String description, Personne pere) {
-        this.id = id;
-        this.nom = nom;
-        this.prenom = prenom;
-        this.description = description;
-        this.pere = pere;
-    }
 
     public void addFils(Personne personne){
         this.fils.add(personne);
--- src/main/java/gui/Identification.java
+++ src/main/java/gui/Identification.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by landschoot on 25/11/16.
@@ -9,9 +10,25 @@ import javax.swing.*;
 public class Identification extends JFrame {
     public Identification(){
         this.setTitle("Identification");
-        this.setSize(400, 500);
+        this.setSize(240, 100);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(buildContentPane());
         this.setVisible(true);
+        this.setResizable(false);
+    }
+
+    private JPanel buildContentPane(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setBackground(Color.white);
+        JLabel label = new JLabel("Entrer votre identifiant :");
+        label.setHorizontalAlignment(JLabel.CENTER);
+        JTextField text = new JTextField(20);
+        JButton button = new JButton("OK");
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(text, BorderLayout.CENTER);
+        panel.add(button, BorderLayout.SOUTH);
+        return panel;
     }
 }
