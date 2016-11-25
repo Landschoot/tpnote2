4dd3e09fe04e7a91b525f8435fe6d591e602f167
Landschoot Ludovic <landschoot@b13p15.fil.univ-lille1.fr>
2016-11-25 10:52:35 +0100

    schema base bd

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -8,10 +8,14 @@ USE personnels;
 -- Table des utilisateurs
 DROP TABLE IF EXISTS PERSONNE;
 CREATE TABLE PERSONNE (
-	id int NOT NULL AUTO_INCREMENT primary key,
-	nom varchar(15) NOT NULL,
-	prenom varchar(15) NOT NULL,
-	evaluation varchar(200) NOT NULL,
-	pere varchar(100) NOT NULL
+  id int NOT NULL AUTO_INCREMENT primary key,
+  nom varchar(15) NOT NULL,
+  prenom varchar(15) NOT NULL,
+  evaluation varchar(200) NOT NULL,
+  pere varchar(100) NOT NULL
 );
 
+/*
+ALTER TABLE PERSONNE
+  ADD CONSTRAINT fk_personne_pere foreign key (pere) REFERENCES PERSONNE(id);
+*/
