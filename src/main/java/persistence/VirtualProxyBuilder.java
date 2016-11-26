0c36fea947ebab7f09a4dfe520a74ebcafd5fd20
Laurent THIEBAULT <thiebault.laurent@gmail.com>
2016-11-26 16:11:01 +0100

    vp

---
+++ src/main/java/persistence/VirtualProxyBuilder.java
@@ -0,0 +1,29 @@
+package persistence;
+
+import java.lang.reflect.*;
+
+public class VirtualProxyBuilder<T> implements InvocationHandler {
+    T realObject = null;
+
+    Factory<T> factory;
+
+    Class<?> iface;
+
+    public VirtualProxyBuilder(Class<?> iface, Factory<T> factory) {
+        this.iface = iface;
+        this.factory = factory;
+    }
+
+    public T getProxy() {
+        @SuppressWarnings("unchecked")
+        T r  = (T) Proxy.newProxyInstance(iface.getClassLoader(), new Class<?>[] { iface }, this);
+        return r;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (realObject == null) {
+            realObject = factory.create();
+        }
+        return method.invoke(realObject, args);
+    }
+}
