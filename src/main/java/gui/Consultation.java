db1875555172eadb6fe6380704d6547af2feb0cb
= <ludovic.landschoot@etudiant.univ-lille1.fr>
2016-11-26 22:56:43 +0100

    Identification

---
+++ src/main/java/gui/Consultation.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by landschoot on 26/11/16.
+ */
+public class Consultation extends JFrame {
+
+    public Consultation(){
+        this.setTitle("Consultation");
+        this.setSize(700, 350);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        this.setContentPane(buildContentPane());
+        this.setVisible(false);
+        this.setResizable(false);
+    }
+}
--- src/main/java/gui/Identification.java
+++ src/main/java/gui/Identification.java
@@ -1,16 +1,17 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * Created by landschoot on 25/11/16.
  * Classe qui représente la fenêtre d'identification d'une personne
  */
 public class Identification extends JFrame {
+    Consultation consultationFrame = new Consultation();
+
     public Identification(){
         this.setTitle("Identification");
-        this.setSize(240, 100);
+        this.setSize(300, 150);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(buildContentPane());
@@ -20,15 +21,27 @@ public class Identification extends JFrame {
 
     private JPanel buildContentPane(){
         JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.setBackground(Color.white);
-        JLabel label = new JLabel("Entrer votre identifiant :");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        JTextField text = new JTextField(20);
-        JButton button = new JButton("OK");
-        panel.add(label, BorderLayout.NORTH);
-        panel.add(text, BorderLayout.CENTER);
-        panel.add(button, BorderLayout.SOUTH);
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("Identification de la personne ;");
+        userLabel.setBounds(10, 10, 250, 25);
+        panel.add(userLabel);
+
+        JLabel passwordLabel = new JLabel("Identifiant");
+        passwordLabel.setBounds(10, 40, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField(20);
+        passwordText.setBounds(100, 40, 160, 25);
+        panel.add(passwordText);
+
+        JButton loginButton = new JButton("login");
+        loginButton.setBounds(10, 80, 80, 25);
+        panel.add(loginButton);
+
+        JButton registerButton = new JButton("aide");
+        registerButton.setBounds(180, 80, 80, 25);
+        panel.add(registerButton);
         return panel;
     }
 }
